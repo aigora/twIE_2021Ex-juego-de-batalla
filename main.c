03e58f99a5cd12f43fb19a207b6f7c9d4fc4a38d
Diego Fernandes <d.falves@alumnos.upm.es>
2021-07-11 12:06:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("##########        Battle  for  Westeros      ##########\n");
+
+    // WesterosConfiguration(); // Aqui se inicia la configuracion del Enemigo (Westeros)
+
+    int option = 0, finishGame = 0, turno = 0;
+
+    char *menu = "\033[1;35m\nMenu:\n1 - Choose my configuration\n" \
+                "2 - Continue Automatically (The game will run until there is a winner)\n" \
+                "3 - Exit game\n\033[0;m";
+
+    // Loop principal
+
+    while (finishGame == 0)
+    {
+        if (option == 0) // Inicio de juego o nuevo turno
+        {
+
+
+        }
+    }
+
+
+
+
+
+
+
+}
 
